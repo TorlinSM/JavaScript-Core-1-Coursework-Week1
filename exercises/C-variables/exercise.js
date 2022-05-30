105f55377817f5edf23fd7f559844504aedf17ed
Serhii <tsn.poppy@gmail.com>
2022-05-30 12:23:34 +0300

    do A, B, C exercises

--- exercises/C-variables/exercise.js
+++ exercises/C-variables/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `greeting`
-
+let greeting = "Hello, Pavel!";
 console.log(greeting);
+let greeting1 = "Hello, Pavel!";
+console.log(greeting1);
+let greeting2 = "Hello, Pavel!";
+console.log(greeting2);
