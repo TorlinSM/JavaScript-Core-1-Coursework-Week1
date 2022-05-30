576a3b4592049572aa897a3202c18d9c3eb19c54
Serhii <tsn.poppy@gmail.com>
2022-05-30 13:28:59 +0300

    do F exercise2.js

--- exercises/F-strings-methods/exercise2.js
+++ exercises/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
-const name = " Daniel  ";
-
+const name = " Serhii  ";
+let nameLength = name.length - 3;
+let message = "My name is " + name.trim() + " and my name is " + nameLength + " characters long";
 console.log(message);
