fb2ef1fa5fc57c0592eb8d1190a4321ad8e03a1f
Serhii <tsn.poppy@gmail.com>
2022-05-30 13:15:27 +0300

    do F exercise.js, not exercise2.js

--- exercises/F-strings-methods/exercise.js
+++ exercises/F-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+let name = "Serhii";
+let nameLength = name.length;
+let message = "My name is " + name + " and my name is " + nameLength + " characters long";
 console.log(message);
