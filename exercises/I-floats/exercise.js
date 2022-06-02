f1382bb0a92a19964b12bea8e0b930b23817b030
Serhii <tsn.poppy@gmail.com>
2022-06-02 11:26:12 +0300

    add I exercise

--- exercises/I-floats/exercise.js
+++ exercises/I-floats/exercise.js
@@ -1,2 +1,17 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+let numberOfStudents = 10;
+let numberOfMentors = 3;
+let Students = "Percentage students: ";
+let Mentors = "Percentage mentors: ";
+let sum = 10 + 3;
+function percentage1(numberOfStudents) {
+    return (100 * numberOfStudents)/sum;
+}
+percentage1 (numberOfStudents);
+function percentage2(numberOfMentors) {
+    return (100 * numberOfMentors)/sum;
+}
+percentage2 (numberOfMentors);
+let message1 = Students + Math.round(percentage1 (numberOfStudents)) + "%";
+let message2 = Mentors + Math.round(percentage2 (numberOfMentors)) + "%";
+console.log(message1);
+console.log(message2);
