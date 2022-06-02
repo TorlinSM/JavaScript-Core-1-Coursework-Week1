7bfe79aba3736aa721eafa21aabc6d01ee687b2c
Serhii <tsn.poppy@gmail.com>
2022-06-02 10:37:52 +0300

    add exercise G

--- exercises/G-numbers/exercise.js
+++ exercises/G-numbers/exercise.js
@@ -1,1 +1,13 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+let numberOfStudents = 10;
+let numberOfMentors = 3;
+let Students = "Number of students: ";
+let Mentors = "Number of Mentors: ";
+let totalsum = "Total numnber of students and mentors: ";
+let sum = 10 + 3;
+let message1 = Students + numberOfStudents;
+let message2 = Mentors + numberOfMentors;
+let message3 = totalsum + sum;
+console.log(message1);
+console.log(message2);
+console.log(message3);
