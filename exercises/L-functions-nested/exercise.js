d0dfe65bb7c4e48b8c8e6110ed3e45160852fbe4
Serhii <tsn.poppy@gmail.com>
2022-06-20 11:52:21 +0300

    fix all mistakes in exercise

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -1,14 +1,23 @@
  
 
-function getAgeInDays(age) {
-    return age * 365;
-  }
-  
-  function createCreeting(name, age) {
-    var ageInDays = getAgeInDays(age);
-    var message =
-      "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
-    return message;
-  }
-  let name = "Daniel";
-  console.log(message);
+
+function toUpperCase(name) {
+  // ... transform name to Upper Case and return it;
+  return name.toUpperCase();
+}
+
+function createGreeting(name) {
+  return "HELLO " + toUpperCase(name);
+}
+
+const greeting1 = createGreeting ("Daniel");
+const greeting2 = createGreeting ("Irina");
+const greeting3 = createGreeting ("Mimi");
+const greeting4 = createGreeting ("Rob");
+const greeting5 = createGreeting ("Yohannes");
+
+console.log(greeting1)
+console.log(greeting2)
+console.log(greeting3)
+console.log(greeting4)
+console.log(greeting5)
